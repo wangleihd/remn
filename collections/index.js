16c9e64445b1bcce385ffec0daf38ca0b5d05c5c
Richard Wang <leix.wang@outlook.com>
2017-04-27 11:22:30 +0800

    cmd: mongoose set.

---
+++ collections/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const url = 'mongodb://192.168.1.11:27017/stu';
+
+mongoose.connect(url);
+
+let db = mongoose.connection;
+
+db.once('open', function(){
+  console.log('connect db ok!');
+})
+
+let Schema = mongoose.Schema;
+
+let userShema = Schema({
+  name: {type: String, default: 'admin' },
+  passwd: {type: String, default: 'admin' }
+});
+
+
+module.exports.user = mongoose.model('user', userShema);
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../collections');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  let user = db.user().save();
   res.send('respond with a resource');
 });
 
